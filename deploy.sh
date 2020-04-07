9b4ffbe632f32c5f92b07441a780a2af3992c907
cuihui2012 <751670441@qq.com>
2020-04-07 16:17:12 +0800

    新增部署文件

--- deploy.sh
+++ deploy.sh
@@ -4,10 +4,8 @@
 ##需要export PROJ_PATH=jenkins任务在部署机器上的路径,已在任务中的shell脚本上配置
 
 ##编译
-cd $PROJ_PATH/app_demo
+cd $PROJ_PATH/dist_app_demo/parentdemo
 mvn clean install -DskipTests
 ##war包部署
-
-
 
 ##重启docker容器
