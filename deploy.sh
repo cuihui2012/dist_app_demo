bf5f9adb7945e6269388a46893441edcb827fc25
cuihui2012 <751670441@qq.com>
2020-04-07 17:07:08 +0800

    提交

--- deploy.sh
+++ deploy.sh
@@ -6,6 +6,9 @@
 ##编译
 cd $PROJ_PATH/dist_app_demo/parentdemo
 mvn clean install -DskipTests
-##war包部署
-##asdasdsadsa
-##重启docker容器
+##jar包部署,此处只部署微服务(provider、consumer)
+cp /root/jenkins/workspace/DeployDistAppDemo/dist_app_demo/providerdemo/target/providerdemo-0.0.1-SNAPSHOT.jar /root/spring-family/provider/
+cp /root/jenkins/workspace/DeployDistAppDemo/dist_app_demo/consumerdemo/target/consumerdemo-0.0.1-SNAPSHOT.jar /root/spring-family/consumer/
+##重启微服务
+sh /root/spring-family/provider/restart.sh
+sh /root/spring-family/consumer/restart.sh
