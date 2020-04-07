f5e944d05bf6c20aeccbe7a4441581157cf4b1ea
cuihui2012 <751670441@qq.com>
2020-04-07 17:24:30 +0800

    提交

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@
 
 ##需要export PROJ_PATH=jenkins任务在部署机器上的路径,已在任务中的shell脚本上配置
 
-##编译
+##编译(微服务在代码上和部署上都应该分开,此处只是做demo,一并打包部署)
 cd $PROJ_PATH/dist_app_demo/parentdemo
 mvn clean install -DskipTests
 ##jar包部署,此处只部署微服务(provider、consumer)
